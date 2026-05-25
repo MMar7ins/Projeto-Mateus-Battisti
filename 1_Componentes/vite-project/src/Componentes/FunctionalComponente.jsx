6ef080b77322492d96535c7f2e1ea5315ba07f12
Marcelo <m.araujo1505@gmail.com>
2026-05-25 19:27:30 -0300

    iniciando o projeto

---
+++ 1_Componentes/vite-project/src/Componentes/FunctionalComponente.jsx
@@ -0,0 +1,12 @@
+function FunctionalComponente () {
+
+    // retorna um objeto com jsx.
+    return (
+        <div>
+            <h2>Testando Componente</h2>;   
+        </div>
+        
+    )
+}
+
+export 
